2157c9906c02cdd3d2c1b02f391b5f3e73ef405f
Cleython-Enginner <cleythonsantos@gmail.com>
2021-08-12 23:44:44 -0300

    Cria constants de Vilões

---
+++ livecoding/src/main/java/com/digitalinnovation/livecoding/constants/VillainsConstant.java
@@ -0,0 +1,7 @@
+package com.digitalinnovation.livecoding.constants;
+
+public class VillainsConstant {
+    public static final String VILLAINS_ENDPOINT_LOCAL="/villains";
+    public static final String ENDPOINT_DYNAMO= "http://localhost:8000";
+    public static final String REGION_DYNAMO= "us-east-1";
+}
